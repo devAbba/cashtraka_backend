c59b671779a643ebe1b07c2c72a44e636ea36761
Victor <victor.abbah@imometrics.com>
2024-07-04 17:04:00 +0100

    defined transaction amount type as float

--- database/migrations/2024_06_11_184548_create_transactions_table.php
+++ database/migrations/2024_06_11_184548_create_transactions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('description');
-            $table->integer('amount');
+            $table->float('amount');
             $table->timestamps();
         });
     }
